e67a2305edb4326c89fead6fd2d7b58525c72b49
Ilya <ilya.tyunin.2457@gmail.com>
2023-10-18 23:05:00 +0300

    small fix

--- main.js
+++ main.js
@@ -1,47 +1,58 @@
-const menuBtn = document.querySelector('.menu__btn')
-const menuList = document.querySelector('.menu__list')
-function toggleMenuVisibility() {
-    menuList.classList.toggle('hide')
-}
-menuBtn.addEventListener('click', toggleMenuVisibility)
-// Скрипт для раскрытия блока portfolio
-
-const portfolioBtn = document.querySelectorAll('[data-portfolio]')
-const aboutBtn = document.querySelectorAll('[data-about]')
-const infoBlock = document.querySelector('.info')
-const portfolioBlock = document.querySelector('.portfolio')
-
-portfolioBtn.forEach((item) => {
-    item.addEventListener('click', () => {
-        infoBlock.style.display = 'none'
-        portfolioBlock.style.display = 'block'
-        menuList.classList.add('hide')
-    })
-})
-
-aboutBtn.forEach((item) => {
-    item.addEventListener('click', () => {
-        infoBlock.style.display = 'flex'
-        portfolioBlock.style.display = 'none'
-        menuList.classList.add('hide')
-    })
-})
-
-const wrapper = document.querySelector('.wrapper')
-const wrapperImg = wrapper.querySelector('img')
-const portfolioImg = portfolioBlock.querySelectorAll('img')
-
-portfolioImg.forEach((item) => {
-    item.addEventListener('click', (e) => {
-        wrapper.style.display = 'flex'
-        wrapperImg.src = item.src
-        document.body.style.overflow = 'hidden'
-    })
-})
-
-wrapper.addEventListener('click', (e) => {
-    if (wrapper === e.target) {
-        wrapper.style.display = 'none'
-        document.body.style.overflow = ''
+document.addEventListener('DOMContentLoaded', () => {
+    const menuBtn = document.querySelector('.menu__btn')
+    const menuList = document.querySelector('.menu__list')
+    function toggleMenuVisibility() {
+        menuList.classList.toggle('hide')
     }
+    menuBtn.addEventListener('click', toggleMenuVisibility)
+    // Скрипт для раскрытия блока portfolio
+
+    const portfolioBtn = document.querySelectorAll('[data-portfolio]')
+    const aboutBtn = document.querySelectorAll('[data-about]')
+    const infoBlock = document.querySelector('.info')
+    const portfolioBlock = document.querySelector('.portfolio')
+
+    const elem = document.querySelector('.images')
+    const msnry = new Masonry(elem, {
+        // options
+        itemSelector: '.images__item',
+        columnWidth: 500,
+        gutter: 10,
+    })
+
+    portfolioBtn.forEach((item) => {
+        item.addEventListener('click', () => {
+            infoBlock.style.display = 'none'
+            portfolioBlock.style.display = 'block'
+            menuList.classList.add('hide')
+            msnry.layout()
+        })
+    })
+
+    aboutBtn.forEach((item) => {
+        item.addEventListener('click', () => {
+            infoBlock.style.display = 'flex'
+            portfolioBlock.style.display = 'none'
+            menuList.classList.add('hide')
+        })
+    })
+
+    const wrapper = document.querySelector('.wrapper')
+    const wrapperImg = wrapper.querySelector('img')
+    const portfolioImg = portfolioBlock.querySelectorAll('img')
+
+    portfolioImg.forEach((item) => {
+        item.addEventListener('click', (e) => {
+            wrapper.style.display = 'flex'
+            wrapperImg.src = item.src
+            document.body.style.overflow = 'hidden'
+        })
+    })
+
+    wrapper.addEventListener('click', (e) => {
+        if (wrapper === e.target) {
+            wrapper.style.display = 'none'
+            document.body.style.overflow = ''
+        }
+    })
 })
